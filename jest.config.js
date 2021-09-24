d10b9d5d8f7962cfd103fc4621ac34494d9ec3c5
Antonio Torres <antonio.torres@williamhill.com>
2021-09-24 16:04:10 +0200

    :rocket: (pipeline) added linting, jest, typescript and prettier
    added scaffolding config - eslint with airbnb, jest with ts, typescriot and prettier

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  resetModules: true,
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)(test).(ts|tsx)'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/__snapshots__/',
+    'coverage',
+    'test/cypress',
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+      'src/**/*.{js,jsx,ts,tsx}',
+  ],
+  coveragePathIgnorePatterns: [
+    '__stories__',
+    'index.ts'
+  ],
+  coverageDirectory: '<rootDir>/coverage',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+};
---
+++ src/__test__/app.test.ts
@@ -0,0 +1,8 @@
+import app from '../app';
+
+describe('this is a example test', () => {
+  it('test jest config', () => {
+    app();
+    expect(true).toBe(true);
+  });
+});
