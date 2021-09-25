786fb584c4e0c41f99f3ddfdbd84a6aa4e52bec7
Antonio Torres <antonio.torres@williamhill.com>
2021-09-25 13:22:11 +0200

    :rocket: (exchange-app) exchageRate test + printCurrency function
    added ExchangeRate unit test and printCurrency function

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,20 +1,32 @@
-import { getCurrencyShort } from './utils';
+import { getCurrencyShort, printCurrency } from './utils';
 import { Currency } from '../types';
 
 describe('utils', () => {
-  it('getCurrencyShor return symbol when exist code if not', async () => {
-    const CurrencyWithSymbol: Currency = {
+  it('getCurrencyShort return symbol when exist code if not', async () => {
+    const currencyWithSymbol: Currency = {
       symbol: '$',
       code: 'USD',
       name: 'United States Dollar',
     };
 
-    const CurrencyWithoutSymbol: Currency = {
+    const currencyWithoutSymbol: Currency = {
       code: 'GBP',
       name: 'British Sterling Pound',
     };
 
-    expect(getCurrencyShort(CurrencyWithSymbol)).toBe('$');
-    expect(getCurrencyShort(CurrencyWithoutSymbol)).toBe('GBP');
+    expect(getCurrencyShort(currencyWithSymbol)).toBe('$');
+    expect(getCurrencyShort(currencyWithoutSymbol)).toBe('GBP');
+  });
+
+  it('printCurrent return currency formated', async () => {
+    const currency: Currency = {
+      symbol: '$',
+      code: 'USD',
+      name: 'United States Dollar',
+    };
+    const precision = 5;
+    const value = 1.1234567;
+
+    expect(printCurrency(precision)(currency)(value)).toBe('1.12346 $');
   });
 });
