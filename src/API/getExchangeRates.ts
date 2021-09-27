d2cf560095aff28e3bd2218f31cafed523bb43e5
Antonio Torres <antonio.torres@williamhill.com>
2021-09-27 10:52:05 +0200

    :rocket: (exchange-app) connect refreshRates and quantities
    connect refreshRates and quantities

--- src/API/getExchangeRates.ts
+++ src/API/getExchangeRates.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-import config from '../../config';
-import { ExchangeRates } from '../../types';
+import config from '../config';
+import { ExchangeRates } from '../types';
 
 const getUrl = () => `${config.exchangeAPIurl}/exchangeRates`;
 
--- webpack/generateExchangeRates.js
+++ webpack/generateExchangeRates.js
@@ -1,7 +1,9 @@
+const randomNumber = (min, max) => Math.random() * (max - min) + min;
+
 const generateExchangeRates = () => ({
   EUR: 1,
-  USD: 1.1716,
-  GBP: 0.8563,
+  USD: 1.1716 + randomNumber(0.02, 0.01),
+  GBP: 0.8563 + randomNumber(0.02, 0.01),
 });
 
 module.exports = { generateExchangeRates };
